b847b8b8aed10ee99956c31a271befdbf34270cc
jakesummer <jakersummer@gmail.com>
2026-06-30 18:09:21 -0400

    feat: add close button to new message modal

--- public/main.js
+++ public/main.js
@@ -1,8 +1,15 @@
 const newMessageButton = document.getElementById("new-message-btn");
 const newMessageModal = document.getElementById("new-message-modal");
 const newMessageForm = document.getElementById("new-message-form");
+const closeModalButton = document.getElementById("close-modal-btn");
 
 newMessageButton.addEventListener("click", () => {
   newMessageForm.reset();
-  newMessageModal.setAttribute("open", true)
-})
+  newMessageModal.showModal();
+  document.body.classList.add("no-scroll");
+});
+
+closeModalButton.addEventListener("click", () => newMessageModal.close());
+newMessageModal.addEventListener("close", () =>
+  document.body.classList.remove("no-scroll"),
+);
